f89867110c413ad715edc9748e05fb521f1f4808
Kuiliang Zhang <z1219202167@outlook.com>
2024-03-31 14:45:07 -0700

    chore: add tooltip for axes

--- src/components/stats/crs-chart/filters/axes-filter.tsx
+++ src/components/stats/crs-chart/filters/axes-filter.tsx
@@ -2,11 +2,16 @@
 
 import { useAtom } from "jotai";
 import { toast } from "sonner";
-import { Checkbox } from "@mantine/core";
+import { Checkbox, Tooltip } from "@mantine/core";
 import { capitalize } from "@/lib/utils";
 import { axesAtom } from "../utils/atoms";
 import { type Axes, axesOptions } from "../utils/type";
 import FilterTitle from "./header";
+
+const tooltips: Record<Axes, string> = {
+  score: "The CRS score of the lowest-ranked candidate invited for the draw",
+  issued: "The number of candidates invited for the draw",
+};
 
 export default function AxesFilter() {
   const [axes, setAxes] = useAtom(axesAtom);
@@ -26,7 +31,16 @@ export default function AxesFilter() {
       <Checkbox.Group value={axes} onChange={handleAxesChange}>
         <div className="my-2 flex gap-x-5">
           {axesOptions.map((axis) => (
-            <Checkbox key={axis} value={axis} label={capitalize(axis)} />
+            <Tooltip
+              multiline
+              w={220}
+              withArrow
+              label={tooltips[axis]}
+              key={axis}
+              events={{ hover: true, focus: true, touch: true }}
+            >
+              <Checkbox key={axis} value={axis} label={capitalize(axis)} />
+            </Tooltip>
           ))}
         </div>
       </Checkbox.Group>
