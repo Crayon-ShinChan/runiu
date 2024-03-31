955df4fd47600121ff14b1c17583faad2c625804
Kuiliang Zhang <z1219202167@outlook.com>
2024-03-31 11:51:22 -0700

    feat: add helpful links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { Calculator } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@mantine/core";
 import Container from "@/components/ui/container";
 import { api } from "@/trpc/server";
 import CRSChart from "../components/stats/crs-chart";
@@ -9,7 +13,37 @@ export default async function Home() {
     <Container>
       <main>
         <CRSChart roundData={roundData} />
+        <div className="px-5">
+          <HelpfulLinks />
+        </div>
       </main>
     </Container>
+  );
+}
+
+function HelpfulLinks() {
+  return (
+    <>
+      <h2 className="mt-6 pb-2 font-semibold text-zinc-900">Helpful Links</h2>
+      <div className="flex gap-x-4">
+        <Button
+          leftSection={
+            <Image src="/maple.svg" alt="maple" height={16} width={16} />
+          }
+        >
+          <Link
+            href="https://www.canada.ca/en/immigration-refugees-citizenship/services/immigrate-canada/express-entry/works.html"
+            target="_blank"
+          >
+            How Express Entry works
+          </Link>
+        </Button>
+        <Button leftSection={<Calculator size={16} />}>
+          <Link href="https://www.lptoronto.com/calc/" target="_blank">
+            Calculate your CRS score
+          </Link>
+        </Button>
+      </div>
+    </>
   );
 }
