3b1564b0608cedc05f0597348bd86a12a65e2485
Kuiliang Zhang <38809606+Crayon-ShinChan@users.noreply.github.com>
2024-04-01 11:21:21 -0700

    fix: get real domain for production for sitemap (#17)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,12 @@
+import { env } from "@/env";
 import { getBaseUrl } from "@/lib/utils";
+
+const baseUrl = env.NEXT_PUBLIC_SITE_URL ?? getBaseUrl();
 
 export default async function sitemap() {
   return [
     {
-      url: getBaseUrl(),
+      url: baseUrl,
       lastModified: new Date(),
       priority: 1,
     },
