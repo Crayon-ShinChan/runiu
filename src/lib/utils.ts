ac4585c8a16d6177c4f096b9f1da44bd362b5cf1
Kuiliang Zhang <z1219202167@outlook.com>
2024-04-01 10:40:12 -0700

    feat: generate dynamic url for sitemap

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,9 @@ export function cn(...inputs: ClassValue[]) {
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function getBaseUrl() {
+  if (typeof window !== "undefined") return window.location.origin;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
