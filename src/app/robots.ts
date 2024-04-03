a8701bfb445b3843715266449f3e88dddcc6d70f
Kuiliang Zhang <z1219202167@outlook.com>
2024-04-02 23:10:50 -0700

    chore: update robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,7 +3,7 @@ import { env } from "@/env";
 import { getBaseUrl } from "@/lib/utils";
 
 const baseUrl = env.NEXT_PUBLIC_SITE_URL ?? getBaseUrl();
-const disallowList = ["/_next/", "/api/", "/public/"];
+const disallowList = ["/api/"];
 
 export default function robots(): MetadataRoute.Robots {
   return {
