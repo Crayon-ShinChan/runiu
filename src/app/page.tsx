444216fe5b94d7da6f74a48006ee3e89b64ec979
MengXi <38809606+Crayon-ShinChan@users.noreply.github.com>
2024-06-18 04:46:28 -0700

    feat: add distribution (#23)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import HelpfulLinks from "@/components/helpful-links";
+import CRSChart from "@/components/stats/crs-chart";
+import DistributionChart from "@/components/stats/distribution-chart";
 import Container from "@/components/ui/container";
 import { api } from "@/trpc/server";
-import CRSChart from "../components/stats/crs-chart";
 
 export default async function Home() {
   const roundData = await api.round.getAll();
@@ -10,6 +11,7 @@ export default async function Home() {
     <Container>
       <main className="pb-8">
         <CRSChart roundData={roundData} />
+        <DistributionChart roundData={roundData} />
         <HelpfulLinks />
       </main>
     </Container>
---
+++ src/components/stats/distribution-chart/utils/spec.ts
@@ -0,0 +1,114 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import type { IHistogramChartSpec } from "@visactor/react-vchart";
+import type { DistributionData } from "./type";
+
+export const getSpec = (data: [DistributionData, ...DistributionData[]]) => {
+  const spec: IHistogramChartSpec = {
+    type: "histogram",
+    data: data[0].data,
+    xField: "start",
+    x2Field: "end",
+    yField: "value",
+    bar: {
+      style: {
+        stroke: "white",
+        lineWidth: 1,
+      },
+    },
+    tooltip: {
+      visible: true,
+      mark: {
+        title: {
+          key: "title",
+          value: "Number of People",
+        },
+        content: [
+          {
+            key: (datum) => datum!.start + "～" + datum!.end,
+            value: (datum) => datum!.value,
+          },
+        ],
+      },
+    },
+    customMark: [
+      {
+        type: "text",
+        dataId: "date",
+        style: {
+          textBaseline: "bottom",
+          dy: 12,
+          fontSize: 14,
+          textAlign: "right",
+          fontWeight: 600,
+          text: (datum) => datum.date,
+          x: (datum, ctx) => {
+            return (ctx.vchart as any).getChart().getCanvasRect()?.width - 50;
+          },
+          y: (datum, ctx) => {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            return (ctx.vchart as any).getChart().getCanvasRect()?.height - 50;
+          },
+          fill: "grey",
+          fillOpacity: 0.5,
+        },
+      },
+    ],
+    crosshair: {
+      trigger: ["click", "hover"],
+      xField: { visible: true, line: { type: "line" } },
+    },
+    axes: [
+      {
+        orient: "left",
+        title: {
+          visible: true,
+          text: "Number of People",
+        },
+        label: {
+          formatMethod: (v) => {
+            return (v as unknown as number) / 1000 + "k";
+          },
+        },
+      },
+    ],
+    player: {
+      type: "continuous",
+      orient: "bottom",
+      dy: 10,
+      auto: true,
+      loop: true,
+      position: "middle",
+      interval: 300,
+      specs: data,
+      slider: {
+        railStyle: {
+          height: 6,
+        },
+      },
+      controller: {
+        backward: {
+          style: {
+            size: 12,
+          },
+        },
+        forward: {
+          style: {
+            size: 12,
+          },
+        },
+        start: {
+          order: 1,
+          position: "end",
+        },
+      },
+    },
+  };
+
+  return spec;
+};
